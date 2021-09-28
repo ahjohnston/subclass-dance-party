3dc6163b7c7262abe5c1f7b35a9b65def89a8d1a
Jessica Yang <jessicaawhy@gmail.com>
2021-09-28 12:57:05 -0700

    Refactor whale dancer file

--- src/whaleDancer.js
+++ src/whaleDancer.js
@@ -2,9 +2,7 @@ var MakeWhaleDancer = function (top, left, timeBetweenSteps) {
   //start with the parent class as a base
   MakeDancer.call(this, top, left, timeBetweenSteps);
   //each new whaleDancer will be represented by a small image of a whale
-  this.$node.attr('src', './lib/whaleFlat.png');
   //add a class to each whale, so that CSS file has access to each whaleDancer
-  this.$node.addClass('whaleDancer');
 };
 
 //set the prototype
@@ -16,20 +14,22 @@ MakeWhaleDancer.prototype.oldStep = MakeDancer.prototype.step;
 //add methods
 MakeWhaleDancer.prototype.step = function () {
   this.oldStep();
-
-  // this.$node.toggle();
+  this.$node.removeClass('dancer');
+  this.$node.addClass('whale');
+  this.$node.attr('src', './lib/whale.png');
 };
 
 //rotate the whale's image by 90 degrees, four times
-MakeWhaleDancer.prototype.flip = function () {
-  this.oldStep();
 
-};
+// MakeWhaleDancer.prototype.flip = function () {
+//   this.oldStep();
 
-//make the whale slap by changing the image
-MakeWhaleDancer.prototype.slap = function () {
-  this.oldStep();
-}
+// };
+
+// //make the whale slap by changing the image
+// MakeWhaleDancer.prototype.slap = function () {
+//   this.oldStep();
+// }
 
 //reassign the constructor
 MakeWhaleDancer.prototype.constructor = MakeWhaleDancer;
